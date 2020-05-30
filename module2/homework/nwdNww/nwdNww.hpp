6ad574680044cde7728ae44d74391f0b841f1f55
kasiasieradzka <kasia.sieradzka.programista@gmail.com>
2020-05-30 23:59:36 +0200

    nwdNww

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -3,9 +3,44 @@
 int NWD(int lhs, int rhs) {
     // TODO: Implement me :)
     return -1;
+
+    if (rhs == 0){
+        return lhs;
+    }
+    if (lhs == 0){
+        return rhs;
+    }
+
+    if (lhs < 0){
+        lhs = -1*lhs;
+    }
+    if (rhs < 0){
+        rhs = -1*rhs;
+    }
+
+    int divider = (lhs > rhs) ? lhs%rhs : rhs%lhs;
+
+    if (divider == 0)
+        return (lhs>rhs) ? rhs : lhs;
+    else
+        return (lhs > rhs) ? NWD(divider, rhs) : NWD (divider, lhs);
+
+    return divider;
 }
 
 int NWW(int lhs, int rhs) {
     // TODO: Implement me :)
     return -1;
+
+    if ((lhs == 0) || (rhs == 0)) {
+        return 0;
+    }
+
+    auto nww = (lhs * rhs) / NWD(lhs, rhs);
+
+    if (nww < 0) {
+        nww = -nww;
+    }
+
+    return nww;
 }

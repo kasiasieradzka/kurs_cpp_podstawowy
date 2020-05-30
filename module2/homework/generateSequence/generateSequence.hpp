80ea2fc157d030d759ee67ba8876e880ca49a047
kasiasieradzka <kasia.sieradzka.programista@gmail.com>
2020-05-30 23:58:42 +0200

    sequence

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -4,4 +4,19 @@
 std::vector<int> generateSequence(int count, int step) {
     // TODO: Implement me :)
     return {};
+
+    if (count <= 0){ 
+        return {};
+    }
+
+    auto firstValue = step;
+
+    std::vector<int> vecSequence;
+
+    for(int i = 0; i < count; i++){
+        vecSequence.push_back(firstValue);
+        firstValue += step;
+    }
+
+    return vecSequence;
 }
